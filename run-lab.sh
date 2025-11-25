5bbdffe86b66b0e6569ec995d80b1ed5f62fa70c
damian-martinezd <54448613+damian-martinezd@users.noreply.github.com>
2025-11-25 17:27:53 -0600

    Update run-lab.sh

--- run-lab.sh
+++ run-lab.sh
@@ -9,6 +9,10 @@ IMAGE_NAME="kitten.png"
 # Create a BUCKET by command line
 gsutil mb -c standard -l $REGION gs://$BUCKET_NAME
 
+# set Uniform for Access control and uncheck
+gsutil uniformbucketlevelaccess set on gs://$BUCKET_NAME/
+
+
 # move image to test the service in Bucket via cloud
 # 1. Download the image
 curl -o $IMAGE_NAME https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.istockphoto.com%2Fphotos%2Fphoto-image-art&psig=AOvVaw1L-wyPdG3DRoaFGjuw6sTG&ust=1764195247174000&source=images&opi=89978449
